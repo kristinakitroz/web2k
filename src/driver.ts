6ad675f7fa4b88ed7f997b07623f57b9af4b2202
kristinakitroz <kristinakitroz@gmail.com>
2024-12-29 21:48:50 +0100

    Prva domasna

---
+++ src/driver.ts
@@ -0,0 +1,11 @@
+//dodaeni
+export interface driver{
+
+    id: number;
+        name: string;
+        iconUrl: string;
+         team: string;
+        category: string;
+        points: number;
+
+}
