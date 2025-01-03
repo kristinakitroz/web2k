b23be466047dcf68969f7493c6e4638340a9272b
kristinakitroz <kristinakitroz@gmail.com>
2025-01-03 19:03:59 +0100

    Vtora domasna angular(aria)

--- src/app/cmp/cmp.component.ts
+++ src/app/cmp/cmp.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { driver } from '../../driver';
 import { CommonModule } from '@angular/common'; 
-
+import { FormsModule } from '@angular/forms'; 
 @Component({
   selector: 'app-comp',
   standalone: true,  
-  imports: [CommonModule],  
+  imports: [CommonModule,FormsModule],  
   templateUrl: './cmp.component.html',
   styleUrls: ['./cmp.component.css']
 })
@@ -23,23 +23,69 @@ export class CmpComponent {
   //}
 
   onDrvView() {
-    
+   
     let link: string | undefined;
-    
+   
 
     if (this.vozac?.iconUrl) {
       link = this.vozac?.iconUrl;
-      
+   
     } else {
       link = "https://www.google.com";
-      
     }
 
     window.open(link, "_blank");
     
   }
+
+expandedIndices: { [key: number]: boolean } = {};
+
+toggleDetails(indeks: number | undefined):void {
+  if (indeks !== undefined) {
+    this.expandedIndices[indeks] = !this.expandedIndices[indeks];
+  }
+}
+isTooltipVisible: { [key: number]: boolean } = {};
+
+toggleTooltip(indeks: number): void {
+  this.isTooltipVisible[indeks] = !this.isTooltipVisible[indeks];
+}
+klasi2() {
+   
+    return {
+      'begin': this.vozac?.category == 'ASD', 
+      'adv': this.vozac?.category == 'EXPERT', 
+      'undr': true 
+    };
+  }
   stilovi() {
     return 'underline';
   
   }
+  ratings: { [key: number]: number } = {};
+  isProcessing: boolean = false; 
+
+  isInvalid(index: number): boolean {
+    const rating = this.ratings[index];
+    return rating < 1 || rating > 10;
+  }
+  
+  onRatingChange(index: number): void {
+    this.isProcessing = true;
+  
+    
+    setTimeout(() => {
+      if (this.isInvalid(index)) {
+        
+        console.log('Nevaliden rejting:', this.ratings[index]);
+      }
+  
+      this.isProcessing = false;
+    }, 2000); 
+  }
+
+getValidIndex(indeks: number | undefined): number {
+  
+  return indeks!==undefined?indeks : 0;
+  }    
 }
